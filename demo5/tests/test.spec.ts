aec2feee82bedbc96942fa6a027ee8e454584670
Hyhello <673089899@qq.com>
2021-12-13 16:05:30 +0800

    fix: 调整细节

--- demo5/tests/test.spec.ts
+++ demo5/tests/test.spec.ts
@@ -1,5 +1,6 @@
 import oneOf from '../src/oneOf';
 import rangeArr from '../src/rangeArr';
+import scrollTo from '../src/scrollTo';
 
 describe('A spec suite', function() {
     it('contains a passing spec', function() {
@@ -12,4 +13,22 @@ describe('A spec suite2', function() {
     it('contains a passing spec2', function() {
         expect(rangeArr(2)).to.eql([undefined, undefined]);
     });
+});
+
+describe('A spec suite3', function() {
+    let $body = document.body,
+    bodyHeight = getComputedStyle($body).getPropertyValue('height');
+    before(function () {
+        $body.style.height = '10000px'
+    })
+    it(`outils.scrollTo() should return true`, function (done) {
+        scrollTo($body, 100).then(() => {
+            expect((document.documentElement || $body).scrollTop === 100);
+            done();
+        });
+    })
+    after(function () {
+        $body.scrollTop = 0;
+        $body.style.height = bodyHeight;
+    })
 });
